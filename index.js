4a6770520c1ad54858354a63f4b1b486678e1c67
Anita <anitalyubc@gmail.com>
2021-08-10 20:12:29 -0700

    Refractored code due to long functions

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ document.getElementById('recipe-submit').addEventListener('click', function (eve
   let cuisine = document.getElementById('recipe-cuisine').value;
   let diet = document.getElementById('recipe-diet').value;
   let number = document.getElementById('recipe-number').value;
-  req.open("GET", "https://pantry-recipes-server.herokuapp.com/search?item=" 
-    + item + "&cuisine=" + cuisine + "&diet=" + diet + "&number=" + number, true);
-  req.addEventListener('load', function() {
+  req.open("GET", "https://pantry-recipes-server.herokuapp.com/search?item=" +
+    item + "&cuisine=" + cuisine + "&diet=" + diet + "&number=" + number, true);
+  req.addEventListener('load', function () {
     if (req.status >= 200 && req.status < 400) {
       var response = JSON.parse(req.responseText);
-      console.log(req);
-      console.log(response);
       if (response.code === 400) {
         document.getElementById('recipe-result').textContent = "No results found. Please try another search.";
       } else {
@@ -25,12 +23,12 @@ document.getElementById('recipe-submit').addEventListener('click', function (eve
   req.send(null);
 });
 
-document.getElementById('recipe-random').addEventListener('click', function(event) {
+document.getElementById('recipe-random').addEventListener('click', function (event) {
   event.preventDefault();
   var req = new XMLHttpRequest();
   let number = document.getElementById('recipe-number').value;
   req.open("GET", "https://pantry-recipes-server.herokuapp.com/randomize?number=" + number, true);
-  req.addEventListener('load', function() {
+  req.addEventListener('load', function () {
     if (req.status >= 200 && req.status < 400) {
       var response = JSON.parse(req.responseText);
       createRecipe(response);
@@ -42,23 +40,31 @@ document.getElementById('recipe-random').addEventListener('click', function(even
   req.send(null);
 });
 
+
+function recipeContainer(response, count) {
+  var container = document.createElement("div");
+  container.classList.add("recipe-container");
+  var id = response[count].id;
+  return container;
+}
+
+function recipeTitle(response, count) {
+  var title = response[count].title;
+  var name = document.createElement("h3")
+  name.classList.add("recipe-title");
+  name.innerHTML = title;
+  return name;
+}
+
 function createRecipe(response) {
   var results = document.getElementById('recipe-result');
   while (results.firstChild) {
     results.removeChild(results.firstChild);
   }
-  console.log(response);
   for (let count = 0; count < response.length; count++) {
-    var container = document.createElement("div");
-    container.classList.add("recipe-container");
 
-    var id = response[count].id;
-    console.log(id);
-
-    var title = response[count].title;
-    var name = document.createElement("h3")
-    name.classList.add("recipe-title");
-    name.innerHTML = title;
+    var container = recipeContainer(response, count);
+    var name = recipeTitle(response, count);
 
     var link = document.createElement('a');
     link.classList.add("recipe-link")
@@ -77,7 +83,6 @@ function createRecipe(response) {
     container.append(link);
     container.append(breaker);
     container.append(img);
-
     results.append(container);
   }
 }
@@ -86,7 +91,7 @@ function linkListen(response) {
   links = document.querySelectorAll('.recipe-link');
   console.log(links);
   links.forEach((link) => {
-    link.addEventListener('click', function(event) {
+    link.addEventListener('click', function (event) {
       let id = this.id;
       let name = this.name;
       recipe = response[name];
